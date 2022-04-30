aba1b2e1fc68cee3cdb9442abdc9347768a88c11
Georgina <gina10111@hotmail.com>
2022-04-29 21:48:29 -0500

    first commit

---
+++ src/MainComponent.js
@@ -0,0 +1,10 @@
+function MainContent(){
+    return (
+        <>
+            <h1>Georgina Alejandra Gámez Melgar</h1>
+            <p>Estudiante de ingeniería en octavo semeste c:</p>
+        </>
+    )
+}
+
+export default MainContent
